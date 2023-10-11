e25e089fc2d1faaae0aadef8e3e59b0a3138b712
aristi125 <michael125lol@gmail.com>
2023-10-10 23:36:29 -0500

    ya esta creado el crud completo con JDBC

--- src/main/java/co/edu/proyectobases/test/EjemploJDBC.java
+++ src/main/java/co/edu/proyectobases/test/EjemploJDBC.java
@@ -6,6 +6,7 @@ import co.edu.proyectobases.repository.Repositorio;
 import co.edu.proyectobases.utils.ConexionBaseDatos;
 
 import java.sql.*;
+import java.util.Date;
 
 public class EjemploJDBC {
     //static Connection conn = null;
@@ -20,30 +21,24 @@ public class EjemploJDBC {
         try (Connection conn = ConexionBaseDatos.getInstance();){
             //vamos a crear la instancia del repositorio
             Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
+
+            System.out.println("=========LISTAR===========");
             repositorio.listar().forEach(p -> System.out.println(p.getNombre()));
-            System.out.println("====================");
-            System.out.println(repositorio.listarPorId(2));
+
+            System.out.println("=========LISTAR POR ID===========");
+            System.out.println(repositorio.listarPorId(2L));
+            System.out.println("=========CREAR PRODUCTO===========");
+            Producto producto = new Producto();
+            producto.setNombre("Iphone X");
+            producto.setPrecio(500);
+            producto.setFechaRegistro(new Date());
+            repositorio.guardar(producto);
+
+            System.out.println("Guardado con exito");
+            repositorio.listar().forEach(System.out::println);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-//        finally {
-//            cerrarConecion();
-//        }
 
     }
-
-    /**
-     * SIRVE PARA CERRAR LA BD
-      */
-//    public static void cerrarConecion(){
-//        try {
-//            if ((result != null) && (stmt != null) && (conn != null )) {
-//                result.close();
-//                stmt.close();
-//                conn.close();
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
---
+++ src/main/java/co/edu/proyectobases/test/EjemploJDBCUpdate.java
@@ -0,0 +1,44 @@
+package co.edu.proyectobases.test;
+
+import co.edu.proyectobases.model.Producto;
+import co.edu.proyectobases.repository.ProductoRepositorioImpl;
+import co.edu.proyectobases.repository.Repositorio;
+import co.edu.proyectobases.utils.ConexionBaseDatos;
+
+import java.sql.*;
+import java.util.Date;
+
+public class EjemploJDBC {
+    //static Connection conn = null;
+    //para listar lo de la BD
+    //debuleve un cursor que esta dentro del resulset
+   // static Statement stmt = null;
+    //static ResultSet result = null;
+    public static void main(String[] args) {
+
+        //SE ENCARGA DE ADMINISTRAR LAS CONECCIONES
+        //Y LOS DIRVES QUE TENEMOS DISPONIBLES
+        try (Connection conn = ConexionBaseDatos.getInstance();){
+            //vamos a crear la instancia del repositorio
+            Repositorio<Producto> repositorio = new ProductoRepositorioImpl();
+
+            System.out.println("=========LISTAR===========");
+            repositorio.listar().forEach(p -> System.out.println(p.getNombre()));
+
+            System.out.println("=========LISTAR POR ID===========");
+            System.out.println(repositorio.listarPorId(2L));
+            System.out.println("=========CREAR PRODUCTO===========");
+            Producto producto = new Producto();
+            producto.setNombre("Iphone X");
+            producto.setPrecio(500);
+            producto.setFechaRegistro(new Date());
+            repositorio.guardar(producto);
+
+            System.out.println("Guardado con exito");
+            repositorio.listar().forEach(System.out::println);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
